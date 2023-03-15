59685690ef8f9db1f689403ee77323656bf27211
Lucas <lucasdamata01@hotmail.com>
2023-03-15 09:00:09 -0300

    Atualização achar posições
    Atualização na função de achar posições, agora utiliza os métodos corretos das classes.

--- Jogo/pecas/Pikeman.py
+++ Jogo/pecas/Pikeman.py
@@ -1,11 +1,14 @@
 import models.Tile as tl
+import models.Board as bd
 
 class Pikeman(tl.Tile):
     def __init__(self, imagem: str, jogador: str):
         super().__init__(imagem, jogador)
     
-    def acharPosicoesPossiveis(self, tamanhoTabuleiro: int) -> None:
+    def acharPosicoesPossiveis(self, tabuleiro: bd.Board) -> None:
         self.posicoesPossiveis.clear()
+
+        tamanhoTabuleiro = tabuleiro.tamanho()
 
         minhaLinha = self.posicao[0]
         minhaColuna = self.posicao[1]
@@ -15,27 +18,33 @@ class Pikeman(tl.Tile):
             if self.direcao:
                 # Se move para cima
                 if minhaLinha-1 >= 0:
-                    self.__salvarPosicao(minhaLinha-1, minhaColuna)
+                    peca = tabuleiro.grade[minhaLinha-1][minhaColuna]
+                    self.encontro((minhaLinha-1, minhaColuna), peca)
                 
             else:
                 # Se move para baixo
                 if minhaLinha+2 <= tamanhoTabuleiro-1:
-                    self.__salvarPosicao(minhaLinha+2, minhaColuna)
+                    peca = tabuleiro.grade[minhaLinha+2][minhaColuna]
+                    self.encontro((minhaLinha+2, minhaColuna), peca)
 
         elif self.lado == 1:
             if self.direcao:
                 # diagonal superior esquerda
                 if minhaLinha-1 >= 0 and minhaColuna-1 >= 0:
-                    self.__salvarPosicao(minhaLinha-1, minhaColuna-1)
+                    peca = tabuleiro.grade[minhaLinha-1][minhaColuna-1]
+                    self.encontro((minhaLinha-1, minhaColuna-1), peca)
                 # diagonal superior esquerda 2 vezes
                 if minhaLinha-2 >= 0 and minhaColuna+2 <= 5:
-                    self.__salvarPosicao(minhaLinha-2, minhaColuna-2)
+                    peca = tabuleiro.grade[minhaLinha-2][minhaColuna-2]
+                    self.encontro((minhaLinha-2, minhaColuna-2), peca)
                 # diagonal superior direita 1 vez
                 if minhaLinha-1 >= 0 and minhaColuna+1 <= 5:
-                    self.__salvarPosicao(minhaLinha-1, minhaColuna+1)
+                    peca = tabuleiro.grade[minhaLinha-1][minhaColuna+1]
+                    self.encontro((minhaLinha-1, minhaColuna+1), peca)
                 # diagonal superior direita 2 vezes
                 if minhaLinha-2 >= 0 and minhaColuna+2 <= 5:
-                    self.__salvarPosicao(minhaLinha-2, minhaColuna+2)
+                    peca = tabuleiro.grade[minhaLinha-2][minhaColuna+2]
+                    self.encontro((minhaLinha-2, minhaColuna+2), peca)
             else:
                 pass
 
