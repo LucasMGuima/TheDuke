641b03922b097896843d2ae24f184506419b746d
Lucas <lucasdamata01@hotmail.com>
2023-03-17 17:36:03 -0300

    Knight Update
    Movimentação do lado ativo completa, começo da movimentação do lado não ativo

--- Jogo/pecas/Knight.py
+++ Jogo/pecas/Knight.py
@@ -9,9 +9,72 @@ class Knight(tl.Tile):
         #Limpar posições antigas
         self.posicoesPossiveis.clear()
 
+        tamanhoTabuleiro = tabuleiro.tamanho
+
         minhaLinha = self.posicao[0]
         minhaColuna = self.posicao[1]
 
         if(self.lado == 0):
             #Lado ativo
+            #Mov. Horizontal
+            if(minhaColuna-1 >= 0):
+                peca = tabuleiro.grade[minhaLinha][minhaColuna-1]
+                self.encontro((minhaLinha-1, minhaColuna), peca)
+            if(minhaColuna+1 <= 5):
+                peca = tabuleiro.grade[minhaLinha][minhaColuna+1]
+                self.encontro((minhaLinha, minhaColuna+1), peca)
             
+            #Mov que usa como base a direção
+            if(self.direcao):
+                #Se move para cima
+                
+                #Mov. para traz
+                if(minhaLinha-1 >= 0):
+                    peca = tabuleiro.grade[minhaLinha-1][minhaColuna]
+                    encontro = self.encontro((minhaLinha-1, minhaColuna), peca)
+                    #So move para o -2 se o -1 estiver livre
+                    if(encontro == False and minhaLinha-2 >= 0):
+                        peca = tabuleiro.grade[minhaLinha-2][minhaColuna]
+                        encontro = self.encontro((minhaLinha-1, minhaColuna), peca)
+                
+                #Salto para a frente
+                if(minhaLinha+2 <= 5):
+                    if(minhaColuna-1 >= 0):
+                        peca = tabuleiro.grade[minhaLinha+2][minhaColuna-1]
+                        self.encontro((minhaLinha+2, minhaColuna-1), peca)
+                    if(minhaColuna+1 <= 5):
+                        peca = tabuleiro.grade[minhaLinha+2][minhaColuna+1]
+                        self.encontro((minhaLinha+2, minhaColuna+1), peca)
+            else:
+                #Se move para baixo
+
+                #Mov. para traz
+                if(minhaLinha+1 <= 5):
+                    peca = tabuleiro.grade[minhaLinha+1][minhaColuna]
+                    encontro = self.encontro((minhaLinha+1, minhaColuna), peca)
+                    #Se move para o +2 se o +1 estiver livre
+                    if(encontro == False and minhaLinha+2 <= 5):
+                        peca = tabuleiro.grade[minhaLinha+2][minhaColuna]
+                        self.encontro((minhaLinha+2, minhaColuna), peca)
+
+                #Salto para frente
+                if(minhaLinha-2 >= 0):
+                    if(minhaColuna-1 >= 0):
+                        peca = tabuleiro.grade[minhaLinha-2][minhaColuna-1]
+                        self.encontro((minhaLinha-2, minhaColuna-1), peca)
+                    if(minhaColuna+1 <= 5):
+                        peca = tabuleiro.grade[minhaLinha-2][minhaColuna+1]
+                        self.encontro((minhaLinha-2, minhaColuna+1), peca)
+        elif(self.lado == 1):
+            #Lado não ativo
+
+            #Mov. depende da direção
+            if(self.direcao):
+                #Se move pra cima
+                for n in range(tamanhoTabuleiro):
+                    p = n+1
+                    if(minhaLinha-p >= 0):
+                        #Percorre o caminho até acabar ou encontrar algo
+                        peca = tabuleiro.grade[minhaLinha-p][minhaColuna]
+                        encontro = self.encontro((minhaLinha-p, minhaColuna), peca)
+                        if(encontro == False): break
