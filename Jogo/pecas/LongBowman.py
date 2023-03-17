fc9170c31f300192153567cfc7002a34871fa5bb
Raphael Shodi <raphael.s.k@hotmail.com>
2023-03-17 01:13:40 -0300

    LongBowman feito

---
+++ Jogo/pecas/LongBowman.py
@@ -0,0 +1,50 @@
+import models.Tile as tl
+import models.Board as bd
+
+class LongBowman(tl.Tile):
+    def __init__(self, imagem: str, jogador: str):
+        super().__init__(imagem, jogador)
+
+    def acharPosicoesPossiveis(self, tabuleiro: bd.Board) -> None:
+        tamanhoTabuleiro = tabuleiro.tamanho
+
+        self.posicoesPossiveis.clear()
+
+        minhaLinha = self.posicao[0]
+        minhaColuna = self.posicao[1]
+
+        if(self.lado == 0):
+            if(self.direcao):
+                #Se move para cima
+                if(minhaLinha-1 >= 0):
+                    peca = tabuleiro.grade[minhaLinha-1][minhaColuna]
+                    self.encontro((minhaLinha-1, minhaColuna), peca)
+            else:
+                #Se move para baixo
+                if(minhaLinha+1 < tamanhoTabuleiro):
+                    peca = tabuleiro.grade[minhaLinha+1][minhaColuna]
+                    self.encontro((minhaLinha+1, minhaColuna), peca)
+
+            #Se move para a direita
+            if(minhaColuna+1 < tamanhoTabuleiro):
+                peca = tabuleiro.grade[minhaLinha][minhaColuna+1]
+                self.encontro((minhaLinha, minhaColuna+1), peca)
+            
+            #Se move para a esquerda
+            if(minhaColuna-1 >= 0):
+                peca = tabuleiro.grade[minhaLinha][minhaColuna-1]
+                self.encontro((minhaLinha, minhaColuna-1), peca)
+
+        elif(self.lado == 1):
+            if(self.direcao):
+                 # diagonal baixo esquerda
+                if(minhaLinha+1 <= tamanhoTabuleiro-1 and minhaColuna-1 >= 0):
+                    peca = tabuleiro.grade[minhaLinha+1][minhaColuna-1]
+                    self.encontro((minhaLinha+1, minhaColuna-1), peca)
+                # diagonal baixo direita
+                if(minhaLinha+1 <= tamanhoTabuleiro-1 and minhaColuna+1 <= tamanhoTabuleiro-1):
+                    peca = tabuleiro.grade[minhaLinha+1][minhaColuna+1]
+                    self.encontro((minhaLinha+1, minhaColuna+1), peca)
+
+    def informacao(self):
+        return super().informacao()
