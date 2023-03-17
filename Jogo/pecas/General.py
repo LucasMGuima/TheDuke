f773ba491747fbe99e730425df3d962ddb0d11e4
Lucas <lucasdamata01@hotmail.com>
2023-03-17 08:15:08 -0300

    General, falta comandar
    Movimentação do General, falta implementar a ação de comando.

--- Jogo/pecas/General.py
+++ Jogo/pecas/General.py
@@ -16,5 +16,97 @@ class General(tl.Tile):
 
         if(self.lado == 0):
             #Lado ativo
-            #TODO
-            return
+            #Mov. Horizontal
+            if(minhaColuna-1 >= 0):
+                peca = tabuleiro.grade[minhaLinha][minhaColuna-1]
+                encontro = self.encontro((minhaLinha, minhaColuna-1), peca)
+                if(not encontro):
+                    if(minhaColuna-2 >= 0):
+                        peca = tabuleiro.grade[minhaLinha][minhaColuna-2]
+                        self.encontro((minhaLinha, minhaColuna-2), peca)
+            if(minhaColuna+1 <= 5):
+                    peca = tabuleiro.grade[minhaLinha][minhaColuna+1]
+                    encontro = self.encontro((minhaLinha, minhaColuna+1), peca)
+                    if(not encontro):
+                        if(minhaColuna+2 <= 0):
+                            peca = tabuleiro.grade[minhaLinha][minhaColuna+2]
+                            self.encontro((minhaLinha, minhaColuna+2), peca)
+            
+            #Mov. Vertical
+            if(minhaLinha-1 >= 0):
+                peca = tabuleiro.grade[minhaLinha-1][minhaColuna]
+                self.encontro((minhaLinha-1, minhaColuna), peca)
+            if(minhaLinha+1 <= 5):
+                peca = tabuleiro.grade[minhaLinha+1][minhaColuna]
+                self.encontro((minhaLinha+1, minhaColuna), peca)
+            
+            #Mov. Diagonal
+            #Varia com adireção
+            if(self.direcao):
+                #Se move para cima
+                if(minhaLinha-2 >= 0):
+                    if(minhaColuna-1 >= 0):
+                        peca = tabuleiro.grade[minhaLinha-2][minhaColuna-1]
+                        self.encontro((minhaLinha-2, minhaColuna-1), peca)
+                    if(minhaColuna+1 <= 5):
+                        peca = tabuleiro.grade[minhaLinha-2][minhaColuna+1]
+                        self.encontro((minhaLinha-2, minhaColuna+1), peca)
+            else:
+                #Se move para baixo
+                if(minhaLinha+2 <= 5):
+                    if(minhaColuna-1 >= 0):
+                        peca = tabuleiro.grade[minhaLinha+2][minhaColuna-1]
+                        self.encontro((minhaLinha+2, minhaColuna-1), peca)
+                    if(minhaColuna+1 <= 5):
+                        peca = tabuleiro.grade[minhaLinha+2][minhaColuna+1]
+                        self.encontro((minhaLinha+2, minhaColuna+1), peca)
+        elif(self.lado == 1):
+            #Lado não ativo
+            #Mov. Horizontal
+            if(minhaColuna-1 >= 0):
+                peca = tabuleiro.grade[minhaLinha][minhaColuna-1]
+                econtro = self.encontro((minhaLinha, minhaColuna-1), peca)
+                #Anda até o -2
+                if(not encontro and minhaColuna-2 >= 0):
+                    peca = tabuleiro.grade[minhaLinha][minhaColuna-2]
+                    self.encontro((minhaLinha, minhaColuna-2), peca)
+            if(minhaColuna+1 <= 5):
+                peca = tabuleiro.grade[minhaLinha][minhaColuna+1]
+                econtro = self.encontro((minhaLinha, minhaColuna+1), peca)
+                #Anda até o +2
+                if(not encontro and minhaColuna+2 >= 0):
+                    peca = tabuleiro.grade[minhaLinha][minhaColuna+2]
+                    self.encontro((minhaLinha, minhaColuna+2), peca)
+            
+            #Mov dependendo da direção
+            if(self.direcao):
+                #Se move pra cima
+                #Mov. Horizontal
+                if(minhaLinha-1 >= 0):
+                    peca = tabuleiro.grade[minhaLinha-1][minhaColuna]
+                    self.encontro((minhaLinha-1, minhaColuna), peca)
+                
+                #Mov. Diagonal
+                if(minhaLinha-2 >= 0):
+                    if(minhaColuna-1 >= 0):
+                        peca = tabuleiro.grade[minhaLinha-2][minhaColuna-1]
+                        self.encontro((minhaLinha-2, minhaColuna-1), peca)
+                    if(minhaColuna+1 <= 5):
+                        peca = tabuleiro.grade[minhaLinha-2][minhaColuna+1]
+                        self.encontro((minhaLinha-2, minhaColuna+1), peca)
+            else:
+                #Se move para baixo
+                #Mov. Horizontal
+                if(minhaLinha+1 <= 5):
+                    peca = tabuleiro.grade[minhaLinha+1][minhaColuna]
+                    self.encontro((minhaLinha+1, minhaColuna), peca)
+
+                #Mov. Diagonal
+                if(minhaLinha+2 <= 5):
+                    if(minhaColuna-1 >= 0):
+                        peca = tabuleiro.grade[minhaLinha+2][minhaColuna-1]
+                        self.encontro((minhaLinha+2, minhaColuna-1), peca)
+                    if(minhaColuna+1 <= 5):
+                        peca = tabuleiro.grade[minhaLinha+2][minhaColuna+1]
+                        self.encontro((minhaLinha+2, minhaColuna+1), peca)
+                
