3548e19c951c39381d408c9199bb36fe3da1e180
Raphael Shodi <raphael.s.k@hotmail.com>
2023-04-21 21:03:31 -0300

    ajeitei a movimentação do footman

--- Jogo/DukeJogo.py
+++ Jogo/DukeJogo.py
@@ -3,6 +3,7 @@ import models.Tile as tl
 import pecas.Duke as pDuke
 import pecas.Assasin as pAssasin
 import pecas.Bowman as pBowman
+import pecas.Footman as pFootman
 
 tamanho = 6
 
@@ -10,20 +11,23 @@ board = bd.Board(6)
 duke = pDuke.Duke("D", "J1")
 assasin = pAssasin.Assasin("A", "J1", False)
 bowman = pBowman.Bowman("B", "J1")
+footman = pFootman.Footman("f", "J2", False)
 
 assasin.posicao = (0,4)
 duke.posicao = (0,3)
 bowman.posicao = (3,2)
+footman.posicao = (2,2)
 
 board.iniciarPeca(duke)
 board.iniciarPeca(assasin)
 board.iniciarPeca(bowman)
+board.iniciarPeca(footman)
 
 #print(duke.informacao(6))
 
 #duke.acharPosicoesPossiveis(board)
 #assasin.acharPosicoesPossiveis(board)
-bowman.acharPosicoesPossiveis(board)
+footman.acharPosicoesPossiveis(board)
 #print(board.posicionarPeca((2, 'E'), assasin))
 #board.posicionarPeca((2, 'E'), assasin)
 #board.posicionarPeca((2, 'B'), bowman)
--- Jogo/pecas/Footman.py
+++ Jogo/pecas/Footman.py
@@ -6,7 +6,9 @@ class Footman(tl.Tile):
         super().__init__(imagem, jogador)
         self.direcao = direcao
 
-    def acharPosicoesPossiveis(self, tamanhoTabuleiro: int) -> None:
+    def acharPosicoesPossiveis(self, tabuleiro: int) -> None:
+        tamanhoTabuleiro = tabuleiro.tamanho
+
         self.posicoesPossiveis.clear()
 
         minhaLinha = self.posicao[0]
@@ -15,30 +17,53 @@ class Footman(tl.Tile):
         if(self.lado == 0):
             #estado inicail
             if(minhaColuna+1 <= 5):
-                self.__salvarPosicao(minhaLinha, minhaColuna+1)
+                #self.__salvarPosicao(minhaLinha, minhaColuna+1)
+                peca = tabuleiro.grade[minhaLinha][minhaColuna+1]
+                self.encontro((minhaLinha, minhaColuna+1), peca)
             if(minhaColuna-1 >= 0):
-                self.__salvarPosicao(minhaLinha, minhaColuna-1)
+                #self.__salvarPosicao(minhaLinha, minhaColuna-1)
+                peca = tabuleiro.grade[minhaLinha][minhaColuna-1]
+                self.encontro((minhaLinha, minhaColuna-1), peca)
             
             if(minhaLinha+1 <= 5):
-                self.__salvarPosicao(minhaLinha+1, minhaColuna)
+                #self.__salvarPosicao(minhaLinha+1, minhaColuna)
+                peca = tabuleiro.grade[minhaLinha+1][minhaColuna]
+                self.encontro((minhaLinha+1, minhaColuna), peca)
+                
             if(minhaLinha-1 >= 0):
-                self.__salvarPosicao(minhaLinha-1, minhaColuna)
+                #self.__salvarPosicao(minhaLinha-1, minhaColuna)
+                peca = tabuleiro.grade[minhaLinha-1][minhaColuna]
+                self.encontro((minhaLinha-1, minhaColuna), peca)
         elif(self.lado  == 1):
             #estado nao-inicial
             if(minhaLinha+1 <= 5):
-                if(minhaColuna+1 <= 5): self.__salvarPosicao(minhaLinha+1, minhaColuna+1)
-                if(minhaColuna-1 >= 0): self.__salvarPosicao(minhaLinha+1, minhaColuna-1)
+                if(minhaColuna+1 <= 5): #self.__salvarPosicao(minhaLinha+1, minhaColuna+1)
+                    peca = tabuleiro.grade[minhaLinha+1][minhaColuna+1]
+                    self.encontro((minhaLinha+1, minhaColuna+1), peca)
+                if(minhaColuna-1 >= 0): #self.__salvarPosicao(minhaLinha+1, minhaColuna-1)
+                    peca = tabuleiro.grade[minhaLinha+1][minhaColuna-1]
+                    self.encontro((minhaLinha+1, minhaColuna-1), peca)
             
             if(minhaLinha-1 >= 0):
-                if(minhaColuna+1 <= 5): self.__salvarPosicao(minhaLinha-1, minhaColuna+1)
-                if(minhaColuna-1 >= 0): self.__salvarPosicao(minhaLinha-1, minhaColuna-1)
+                if(minhaColuna+1 <= 5): #self.__salvarPosicao(minhaLinha-1, minhaColuna+1)
+                    peca = tabuleiro.grade[minhaLinha-1][minhaColuna+1]
+                    self.encontro((minhaLinha-1, minhaColuna+1), peca)
+                if(minhaColuna-1 >= 0): #self.__salvarPosicao(minhaLinha-1, minhaColuna-1)
+                    peca = tabuleiro.grade[minhaLinha-1][minhaColuna-1]
+                    self.encontro((minhaLinha-1, minhaColuna-1), peca)
 
             if(self.direcao):
                 #Se move pra cima
-                if(minhaLinha-2 <= 0): self.__salvarPosicao(minhaLinha-2, minhaColuna)
+                if(minhaLinha-2 <= 0): #self.__salvarPosicao(minhaLinha-2, minhaColuna)
+                    peca = tabuleiro.grade[minhaLinha-2][minhaColuna]
+                    self.encontro((minhaLinha-2, minhaColuna), peca)
             else:
                 #Se move pra baixo
-                if(minhaLinha+2 >= 5): self.__salvarPosicao(minhaLinha+2, minhaColuna) 
+                if(minhaLinha+2 >= 5): #self.__salvarPosicao(minhaLinha+2, minhaColuna) 
+                    peca = tabuleiro.grade[minhaLinha+2][minhaColuna]
+                    self.encontro((minhaLinha+2, minhaColuna), peca)
+        print(self.posicoesPossiveis)
+            
 
     def informacao(self):
         return super().informacao()
